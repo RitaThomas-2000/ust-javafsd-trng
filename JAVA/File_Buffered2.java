0ff8dfff81d010cd48dc2739688dc40a0cbc26a1
RitaThomas-2000 <112387820+RitaThomas-2000@users.noreply.github.com>
2022-09-13 11:22:36 +0530

    Update File_Buffered2.java

--- JAVA/File_Buffered2.java
+++ JAVA/File_Buffered2.java
@@ -7,7 +7,7 @@ public class File_Buffered2 {
 	public static void main(String args[]) {
 		char[] array=new char[100];
 		try {
-			FileReader file=new FileReader("input.txt");
+			FileReader file=new FileReader("C:\\Users\\ustjavafsdb220\\input.txt");
 			BufferedReader input=new BufferedReader(file);
 			input.read(array);
 			System.out.println("Data in the file: ");
