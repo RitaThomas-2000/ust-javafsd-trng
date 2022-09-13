9c79e3de348450ea34a9279d0fa290abb903772a
RitaThomas-2000 <112387820+RitaThomas-2000@users.noreply.github.com>
2022-09-13 11:25:16 +0530

    Update File_Example4.java

--- JAVA/File_Example4.java
+++ JAVA/File_Example4.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class File_Example4 {
 	public static void main(String args[]) {
-		File file=new File("file.txt");
+		File file=new File("C:\\Users\\ustjavafsdb220\\file.txt");
 		boolean value=file.delete();
 		if(value) {
 			System.out.println("The File is deleted.");
