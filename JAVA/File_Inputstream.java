e38176b89bf41882efbca194f64d0d1f99bf625b
RitaThomas-2000 <112387820+RitaThomas-2000@users.noreply.github.com>
2022-09-13 11:25:40 +0530

    Update File_Inputstream.java

--- JAVA/File_Inputstream.java
+++ JAVA/File_Inputstream.java
@@ -7,7 +7,7 @@ public class File_Inputstream {
 	public static void main(String args[]) {
 		byte array[]=new byte[100];
 		try {
-			InputStream input=new FileInputStream("input.txt");
+			InputStream input=new FileInputStream("C:\\Users\\ustjavafsdb220\\input.txt");
 			System.out.println("Available bytes in the file: "+input.available());
 			input.read(array);
 			System.out.println("Data read from the file: ");
