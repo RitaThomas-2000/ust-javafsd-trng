d7246698dc3593fbcda6269354bd78e678abd1ac
RitaThomas-2000 <112387820+RitaThomas-2000@users.noreply.github.com>
2022-09-22 15:02:14 +0530

    Add files via upload

---
+++ JAVA/JavaStreamExample2.java
@@ -0,0 +1,33 @@
+package Streams;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+
+class Product1{
+	int id;
+	String name;
+	float price;
+	public Product1(int id,String name,float price) {
+		this.id=id;
+		this.name=name;
+		this.price=price;
+	}
+}
+
+public class JavaStreamExample2 {
+	public static void main(String[] args) {
+		List<Product1> productsList=new ArrayList<Product1>();
+		productsList.add(new Product1(1,"HP Laptop",25000f));
+		productsList.add(new Product1(2,"Dell Laptop",30000f));
+		productsList.add(new Product1(3,"Lenevo Laptop",28000f));
+		productsList.add(new Product1(4,"Sony Laptop",28000f));
+		productsList.add(new Product1(5,"Apple Laptop",90000f));
+		Float totalPrice=productsList.stream().map(product->product.price).reduce(0.0f,(sum,price)->sum+price);
+		System.out.println(totalPrice);
+		Float totalPrice2=productsList.stream().map(product->product.price).reduce(0.0f,Float::sum);
+		System.out.println(totalPrice2);
+		
+		
+		
+	}
+}
