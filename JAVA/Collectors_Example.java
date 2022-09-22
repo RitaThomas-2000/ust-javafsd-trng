29db2d294539b0435d204c59a9e02d32a29cf8b1
RitaThomas-2000 <112387820+RitaThomas-2000@users.noreply.github.com>
2022-09-22 15:01:22 +0530

    Add files via upload

---
+++ JAVA/Collectors_Example.java
@@ -0,0 +1,17 @@
+package Predicate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Collectors_Example {
+	public static void main(String[] args) {
+		List<String> names=Arrays.asList("Jon","Ajeet","Steve","Ajeet","Jon","Ajeet");
+		Map<String,Long>map=names.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+		System.out.println(map);
+		
+		
+	}
+}
