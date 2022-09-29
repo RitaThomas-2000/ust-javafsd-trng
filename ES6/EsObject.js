a2e32901d3fbc8ee0a1706501a0abe20e1474f79
RitaThomas-2000 <112387820+RitaThomas-2000@users.noreply.github.com>
2022-09-29 17:16:27 +0530

    Add files via upload

---
+++ ES6/EsObject.js
@@ -0,0 +1,5 @@
+var student={
+    name:"Rita", //don't have datatypes
+    age:22
+}
+console.log(student);
---
+++ ES6/while.js
@@ -0,0 +1,8 @@
+var n=1;
+while(n<=7){
+    console.log("n="+n);
+    if(n==4){
+        break;
+    }
+    n++;
+}
